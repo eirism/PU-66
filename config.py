633ca5c6bc4f89cf612d817b159b1bd1780ead65
Eirik Smithsen <eirik.1996@hotmail.com>
2017-02-20 08:53:51 +0100

    Adds default config values

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-PORT = int(os.environ['PORT'])
-SECRET_KEY = os.environ['SECRET_KEY']
+PORT = int(os.environ.get('PORT', 5000))
+SECRET_KEY = os.environ.get('SECRET_KEY', 'change-this-for-production')
 DEBUG = os.environ.get('DEBUG', 0)
 SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
