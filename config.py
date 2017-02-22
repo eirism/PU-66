cdba8c550adbd5b304da8edd05a31dc9fdb5c825
Eirik Smithsen <eirik.1996@hotmail.com>
2017-02-22 10:47:11 +0100

    Adds persistence and sending of feedback
    Lecture sessions and counts of each feedback type are saved in the DB.
    Adds socketio handlers to process incoming events from students and lectures.

--- config.py
+++ config.py
@@ -6,3 +6,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'change-this-for-production')
 DEBUG = os.environ.get('DEBUG', 0)
 SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+BUTTON_ACTIONS = [
+    ['action_slow', 'To slow'],
+    ['action_fast', 'To fast'],
+    ['action_easy', 'This is easy'],
+    ['action_hard', 'This is hard']
+]
--- iris/views.py
+++ iris/views.py
@@ -1,8 +1,8 @@
-from flask import redirect
-from flask import render_template
-from flask import url_for
-from flask_socketio import emit
+from flask import render_template, redirect, url_for
+from flask_socketio import emit, join_room, rooms
 from iris import app, models, db, socketio
+
+COURSE_ID = 1
 
 
 @app.route('/')
@@ -13,7 +13,20 @@ def index():
 
 @app.route('/student')
 def student():
-    return 'Hello, student'
+    return redirect(url_for('student_feedback', course=1))
+
+
+@app.route('/student/<course>')
+def student_feedback(course):
+    # TODO: Get course ID from database, course name = course
+    course_id = COURSE_ID
+    l_session = get_lecture_session(course_id)
+    counts = dict()
+    actions = app.config['BUTTON_ACTIONS']
+    for action in actions:
+        s_feedback = get_session_feedback(l_session.session_id, action[0])
+        counts[action[0]] = s_feedback.count
+    return render_template('chat.html', course_id=course_id, counts=counts, actions=actions)
 
 
 @app.route('/lecturer')
@@ -26,19 +39,58 @@ def session_control():
     return render_template('lecturer_session.html')
 
 
-@app.route('/chat')
-def chat():
-    return render_template('chat.html')
+@socketio.on('student_send')
+def handle_student_send(message):
+    course_id = message['course_id']
+    if course_id not in rooms():
+        return
+    l_session = get_lecture_session(course_id)
+    if l_session.active:
+        action = message['button']
+        s_feedback = get_session_feedback(l_session.session_id, action)
+        s_feedback.count += 1
+        db.session.add(s_feedback)
+        db.session.commit()
+        emit('lecturer_recv', {'button': [action, s_feedback.count]}, room=course_id)
 
 
-@socketio.on('my_event')
-def handle_text(message):
-    new_text = models.Text(content=message)
-    db.session.add(new_text)
+@socketio.on('lecturer_send')
+def handle_lecturer_send(message):
+    course_id = message['course_id']
+    if course_id not in rooms():
+        return
+    l_session = get_lecture_session(course_id)
+    new_state = message['session_control']
+    if new_state == 'start' and not l_session.active:
+        old_feedbacks = models.SessionFeedback.query.filter_by(session_id=l_session.session_id)
+        for feedback in old_feedbacks.all():
+            emit('lecturer_recv', {'button': [feedback.action_name, 0]}, room=course_id)
+            db.session.delete(feedback)
+        db.session.commit()
+        l_session.active = True
+    elif new_state == 'stop':
+        l_session.active = False
+    db.session.add(l_session)
     db.session.commit()
-    emit('brd_text', message, broadcast=True)
 
 
-@socketio.on('connect')
-def client_connect():
-    print('Client connected')
+@socketio.on('join')
+def client_connect(message):
+    join_room(message['course_id'])
+
+
+def get_lecture_session(course_id):
+    return get_model_or_create(models.LectureSession, (course_id, ))
+
+
+def get_session_feedback(session_id, action_name):
+    return get_model_or_create(models.SessionFeedback, (session_id, action_name))
+
+
+def get_model_or_create(model, parameters):
+    retrieved_model = model.query.get(parameters)
+    if retrieved_model is None:
+        retrieved_model = model(*parameters)
+        db.session.add(retrieved_model)
+        db.session.commit()
+    return retrieved_model
