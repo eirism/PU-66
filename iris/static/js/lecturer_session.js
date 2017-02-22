4bddc73453a764202818bf42978d6889a4642509
Mattis52 <Mathias.s.muller@gmail.com>
2017-02-22 12:20:47 +0100

    Added socket functions to lecturer.js/.html

--- iris/static/js/lecturer_session.js
+++ iris/static/js/lecturer_session.js
@@ -3,13 +3,28 @@
  */
 const button_start = document.getElementById("button_start");
 const button_stop = document.getElementById("button_stop");
+let socket = io();
+socket.emit('join', {'course_id': courseID});
 
 button_start.onclick = function () {
     button_start.disabled = true;
     button_stop.disabled = false;
+
+    console.log("SESSION START");
+    let data = {'session_control': 'start', 'course_id': courseID};
+    socket.emit('lecturer_send', data);
 }
 
 button_stop.onclick = function () {
     button_start.disabled = false;
     button_stop.disabled = true;
+
+    console.log("SESSION STOP");
+    let data = {'session_control': 'stop', 'course_id': courseID};
+    socket.emit('lecturer_send', data);
 }
+
+socket.on('lecturer_recv', function (msg) {
+    console.log(msg['action']);
+    $('#text_' + msg['action'][0]).html(msg['action'][1]);
+});
