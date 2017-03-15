3048ba4efaa7fe75f16ea1d2cea2c8b51776d0df
sardred <andelar96@gmail.com>
2017-03-15 14:00:50 +0100

    Flakes; eight of them

--- db_migrate.py
+++ db_migrate.py
@@ -15,8 +15,7 @@ def migrate_db():
     tmp_module = imp.new_module('old_model')
     old_model = api.create_model(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
     exec(old_model, tmp_module.__dict__)
-    script = api.make_update_script_for_model(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO, tmp_module.meta,
-                                              db.metadata)  # noqa
+    script = api.make_update_script_for_model(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO, tmp_module.meta, db.metadata)  # noqa
     open(migration, "wt").write(script)
     api.upgrade(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
     v = api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
