71ca605ba580b4f9352f798d62d822d8195fd01b
atlantees <jensen.sondre@gmail.com>
2017-03-22 14:35:45 +0100

    Minor changes to codestyle

--- iris/similarity.py
+++ iris/similarity.py
@@ -1,6 +1,7 @@
 from gensim import corpora, models, similarities
 from collections import defaultdict
 
+
 def similarity(inputTexts, inputText, threshold):
     documents = inputTexts
 
@@ -12,44 +13,45 @@ def similarity(inputTexts, inputText, threshold):
     # Remove words that appear only once
     frequency = defaultdict(int)
     for text in texts:
-         for token in text:
-             frequency[token] += 1
+        for token in text:
+            frequency[token] += 1
 
     texts = [[token for token in text if frequency[token] > 1]
-              for text in texts]
+             for text in texts]
 
     dictionary = corpora.Dictionary(texts)
-    #Store the dictionary??
+    # Store the dictionary??
 
     corpus = [dictionary.doc2bow(text) for text in texts]
     # store to disk, for later use??
 
     lsi = models.LsiModel(corpus, id2word=dictionary, num_topics=2)
 
-    #Doc is the input we compare to corpus for similarities
+    # Doc is the input we compare to corpus for similarities
     doc = str(inputText)
     vec_bow = dictionary.doc2bow(doc.lower().split())
-    vec_lsi = lsi[vec_bow] # convert the query to LSI space
+    vec_lsi = lsi[vec_bow]  # convert the query to LSI space
     index = similarities.MatrixSimilarity(lsi[corpus])
     sims = index[vec_lsi]
-    #print(list(enumerate(sims)))
+    # print(list(enumerate(sims)))
 
     sims = sorted(enumerate(sims), key=lambda item: -item[1])
     print(sims)
 
-    #Create a list of questions with higher cosine similiarty than threshold
+    # Create a list of questions with higher cosine similiarty than threshold
     similiarQuestions = []
     for sim in sims:
-        if sim[1]>threshold:
+        if sim[1] > threshold:
             similiarQuestions.append(inputTexts[sim[0]])
     return similiarQuestions
 
+
 documents = ["This is not a test question related to the testing",
-                 "Python is great question",
-                 "A python student asked a super question this great evening",
-                 "Great python lecture"]
+             "Python is great question",
+             "A python student asked a super question this great evening",
+             "Great python lecture"]
 
 compare = "This is test question"
 
-a = similarity(documents,compare, 0.8)
+a = similarity(documents, compare, 0.8)
 print(a)
