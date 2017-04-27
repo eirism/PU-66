a493241d8cbcdcfa24f4a05086c4c9a54ba36a6a
Eirik Smithsen <eirik.1996@hotmail.com>
2017-04-27 05:29:40 +0200

    Add and refactor tests
    Adds some new tests (coverage is 95%) and refactor a bit.
    The new tests actually tests some useful "paths"/behaviours.

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -1,3 +1,11 @@
+def get_socketio_args(response):
+    return response['args'][0]
+
+
+def get_question_group(response):
+    return get_socketio_args(response)['question'][1]
+
+
 def test_init(app, session, client, client_socketio):
     """Tests that the app is initialized correctly (no errors)"""
     pass
@@ -34,7 +42,7 @@ def test_create_course(logged_in_user, client_socketio):
     client_socketio.emit('lecturer_course_new_send',
                          {'code': course_code, 'name': course_name})
     response = client_socketio.get_received()[0]
-    args = response['args'][0]
+    args = get_socketio_args(response)
     assert args['code'] == course_code
     assert args['name'] == course_name
 
@@ -55,6 +63,28 @@ def test_socketio_room_not_joined(client_socketio):
     assert len(client_socketio.get_received()) == 0
 
 
+def test_socketio_stop(logged_in_user, client_socketio):
+    client_socketio.emit('join', {'course_id': 1})
+    client_socketio.emit('lecturer_send', {'course_id': 1, 'session_control': 'start'})
+    client_socketio.emit('lecturer_send', {'course_id': 1, 'session_control': 'stop'})
+    # empty received buffer
+    client_socketio.get_received()
+    client_socketio.emit('student_send', {'course_id': 1, 'action': 'slow'})
+    assert len(client_socketio.get_received()) == 0
+
+
+def test_socketio_start_stop(logged_in_user, client_socketio):
+    from iris import models
+    client_socketio.emit('join', {'course_id': 1})
+    client_socketio.emit('lecturer_send', {'course_id': 1, 'session_control': 'start'})
+    client_socketio.emit('student_send', {'course_id': 1, 'action': 'slow'})
+    client_socketio.emit('lecturer_send', {'course_id': 1, 'session_control': 'stop'})
+    client_socketio.emit('lecturer_send', {'course_id': 1, 'session_control': 'start'})
+    l_session = models.LectureSession.query.filter_by(course_id=1).first().session_id
+    feedbacks = models.SessionFeedback.query.filter_by(session_id=l_session).all()
+    assert len(feedbacks) == 0
+
+
 def test_socketio_keywords(logged_in_user, client_socketio):
     pre_response = 'Objects are from OOP'
     client_socketio.emit('join', {'course_id': 1})
@@ -63,12 +93,42 @@ def test_socketio_keywords(logged_in_user, client_socketio):
                                                   'keywords': 'objects',
                                                   'response': pre_response})
     client_socketio.emit('student_send', {'course_id': 1, 'question': 'What are objects?'})
-    response = client_socketio.get_received()[-1]['args'][0]['question'][2]
+    response = get_socketio_args(client_socketio.get_received()[-1])['question'][2]
     assert pre_response == response
 
 
-def get_question_group(response):
-    return response['args'][0]['question'][1]
+def test_responses_after_question(logged_in_user, client_socketio):
+    from iris import models
+    pre_response = 'Objects are from OOP'
+    course_id = 1
+    question = 'What are objects?'
+    client_socketio.emit('join', {'course_id': course_id})
+    client_socketio.emit('lecturer_send', {'course_id': course_id,
+                                           'session_control': 'start'})
+    client_socketio.emit('student_send', {'course_id': course_id,
+                                          'question': question})
+    client_socketio.emit('lecturer_keyword_new', {'course_id': course_id,
+                                                  'keywords': 'objects',
+                                                  'response': pre_response})
+    l_session = models.LectureSession.query.filter_by(course_id=course_id).first()
+    q = models.Questions.query.filter_by(session_id=l_session.session_id,
+                                         question=question).first()
+    assert pre_response == q.response
+
+
+def test_socketio_update_keyword(logged_in_user, client_socketio):
+    pre_response = 'Objects are from OOP'
+    client_socketio.emit('join', {'course_id': 1})
+    client_socketio.emit('lecturer_send', {'course_id': 1, 'session_control': 'start'})
+    client_socketio.emit('lecturer_keyword_new', {'course_id': 1,
+                                                  'keywords': 'objects',
+                                                  'response': 'Garbage'})
+    client_socketio.emit('lecturer_keyword_new', {'course_id': 1,
+                                                  'keywords': 'objects',
+                                                  'response': pre_response})
+    client_socketio.emit('student_send', {'course_id': 1, 'question': 'What are objects?'})
+    response = get_socketio_args(client_socketio.get_received()[-1])['question'][2]
+    assert pre_response == response
 
 
 def test_socketio_send_student(logged_in_user, client_socketio):
@@ -77,15 +137,19 @@ def test_socketio_send_student(logged_in_user, client_socketio):
     client_socketio.emit('student_send', {'course_id': 1, 'action': 'slow'})
     lecturer_response = client_socketio.get_received()[-1]
     assert lecturer_response['name'] == 'lecturer_recv'
-    assert lecturer_response['args'][0] == {'action': ['slow', 1]}
+    assert get_socketio_args(lecturer_response) == {'action': ['slow', 1]}
     q1 = 'This is a long question.'
     q2 = 'Then how about this? Is it long enough?'
     q3 = 'How are you? I am fine.'
+    q4 = 'This is another long question.'
     client_socketio.emit('student_send', {'course_id': 1, 'question': q1})
     g1 = get_question_group(client_socketio.get_received()[-1])
     client_socketio.emit('student_send', {'course_id': 1, 'question': q2})
     g2 = get_question_group(client_socketio.get_received()[-1])
     client_socketio.emit('student_send', {'course_id': 1, 'question': q3})
     g3 = get_question_group(client_socketio.get_received()[-1])
-    # Check that the previous questions aren't grouped together
+    client_socketio.emit('student_send', {'course_id': 1, 'question': q4})
+    g4 = get_question_group(client_socketio.get_received()[-1])
+    # Check that the three first questions aren't grouped together
     assert g1 != g2 and g2 != g3 and g1 != g3
+    assert g1 == g4
